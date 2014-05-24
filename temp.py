e1833e93d45f67c1f693b64bb5b873d75b1b5dc9
Daniel Harlea <pi@green-lands.(none)>
2014-05-24 19:30:41 +0000

    Initial commit - added python script that reads the input from rpi shield pin 0 and converts to Celsius degrees

---
+++ temp.py
@@ -0,0 +1,30 @@
+import spidev
+import time
+import logging
+import logging.config
+import json
+
+logging.config.fileConfig("config/logging.json")
+logger = logging.getLogger("temp")
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+def readadc(adcnum):
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    r = spi.xfer2([1,(8+adcnum)<<4,0])
+    adcout = ((r[1]&3) << 8) + r[2]
+    return adcout
+
+adcInput = 0 # Pin 0
+logging.info("Reading temperature level from pin-0 :")
+
+while True:
+    value = readadc(adcInput)
+    voltage = value * 3.3
+    voltage /= 1024.0
+    tempCelsius = (voltage-0.5)*100
+    logging.info("Temp: %f", tempCelsius)
+    time.sleep(2)
+
